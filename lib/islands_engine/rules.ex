3456edf6edb2cc8d5d2a19ed151cbc096645662c
Marcos Brizeno <marcos.uece.comp@gmail.com>
2022-10-11 14:29:43 -0500

    Handle islands set for players

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -20,36 +20,55 @@ defmodule IslandsEngine.Rules do
   Updates the `rules` state based on the `action` passed.
 
   ## Examples
-      iex> {:ok, rules} = Rules.check(Rules.new(), :add_player)
-      {:ok, rules}
+      iex> Rules.check(Rules.new(), :add_player)
+      {:ok, %Rules{state: :players_set, player1: :islands_not_set, player2: :islands_not_set}}
 
       iex> Rules.check(Rules.new(), :invalid_action)
       :error
 
-  Setting player positions
-      iex> rules = Rules.new()
-      ...> rules = %{rules | state: :player_set}
-      ...> {:ok, rules} = Rules.check(rules, {:position_islands, :player1})
-      ...> {:ok, rules} = Rules.check(rules, {:position_islands, :player2})
-      {:ok, rules}
+  Positioning islands
+      iex> rules = %{Rules.new() | state: :players_set}
+      ...> Rules.check(rules, {:position_islands, :player1})
+      {:ok, %Rules{state: :players_set, player1: :islands_not_set, player2: :islands_not_set}}
 
-      iex> rules = Rules.new()
-      ...> rules = %{rules | state: :player_set}
-      ...> rules = %{rules | player1: :islands_set}
+      iex> rules = %{Rules.new() | state: :players_set, player1: :islands_set}
       ...> Rules.check(rules, {:position_islands, :player1})
       :error
+
+  Setting player islands
+      iex> rules = %{Rules.new() | state: :players_set}
+      ...> Rules.check(rules, {:set_islands, :player1})
+      {:ok, %Rules{state: :players_set, player1: :islands_set, player2: :islands_not_set}}
+
+      iex> rules = %{Rules.new() | state: :players_set}
+      ...> {:ok, rules} = Rules.check(rules, {:set_islands, :player1})
+      ...> Rules.check(rules, {:set_islands, :player2})
+      {:ok, %Rules{state: :player1_turn, player1: :islands_set, player2: :islands_set}}
   """
   @spec check(Rules.t(), any) :: :error | {:ok, Rules.t()}
   def check(%Rules{state: :initialized} = rules, :add_player) do
     {:ok, %Rules{rules | state: :players_set}}
   end
 
-  def check(%Rules{state: :player_set} = rules, {:position_islands, player}) do
+  def check(%Rules{state: :players_set} = rules, {:position_islands, player}) do
     case Map.fetch!(rules, player) do
       :islands_set -> :error
       :islands_not_set -> {:ok, rules}
     end
   end
 
+  def check(%Rules{state: :players_set} = rules, {:set_islands, player}) do
+    rules = Map.put(rules, player, :islands_set)
+
+    case both_players_islands_set?(rules) do
+      true -> {:ok, %Rules{rules | state: :player1_turn}}
+      false -> {:ok, rules}
+    end
+  end
+
   def check(_state, _action), do: :error
+
+  defp both_players_islands_set?(rules) do
+    rules.player1 == :islands_set && rules.player2 == :islands_set
+  end
 end
